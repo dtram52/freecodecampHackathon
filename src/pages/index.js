d86b47751955774e6f1b83ac12bd23081d0ce480
dtram52 <du.tram90@gmail.com>
2018-09-01 01:48:40 -0700

    some small customization

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,11 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hello World</h1>
+    <p>My name is <em>Du Tram</em> and I'm very excited to attend the freecodecamp hackathon.</p>
+
+    <p>I have been studying as a post-bac CS student while maintaining my full-time job as a Research Manager at a Financial Service firm. </p>
+    <p>Yes I will something great!</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
