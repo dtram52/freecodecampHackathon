1c09b22e8464067f62861779d529c990ab80835d
dtram52 <du.tram90@gmail.com>
2018-09-01 01:52:42 -0700

    some customization

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hello World</h1>
-    <p>My name is <em>Du Tram</em> and I'm very excited to attend the freecodecamp hackathon.</p>
+    <p>My name is <em>Du Tram</em> and I am very excited to attend the freecodecamp hackathon.</p>
 
     <p>I have been studying as a post-bac CS student while maintaining my full-time job as a Research Manager at a Financial Service firm. </p>
     <p>Yes I will something great!</p>
